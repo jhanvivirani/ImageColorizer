a8ca66ddb6e3ff6a34b886ed06c18e9e62fb7574
jhanvivirani <jhanvi.virani@rutgers.edu>
2018-12-12 15:36:40 -0500

    error: expected dense_2 to have shape (None,1) but got array with shape (2,3)

--- colorizer.py
+++ colorizer.py
@@ -1,19 +1,26 @@
 import tensorflow as tf
-from tensorflow.keras import layers
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras import layers
 
-model = tf.keras.Sequential([
-    layers.Flatten(9),
-    layers.Dense(64, activation=tf.nn.relu),
-    layers.Dense(64, activation=tf.nn.relu),
+grayScale = [[1,2,3,4,5,6,7,8,9],[12,24,34,4,52,63,74,85,978]]
+middlePixel = [[1,2,3], [23,3,12]]
+testingGray = grayScale
+testingMiddlePixel = middlePixel
+
+print("making model...")
+model = Sequential([
+    layers.Dense(10, input_dim = 9, activation=tf.nn.relu),
     layers.Dense(3, activation=tf.nn.sigmoid)
 ])
 
+print("model made")
+print("compiling model...")
 model.compile(optimizer=tf.train.AdamOptimizer(), 
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
-
+print("Fitting")
 model.fit(grayScale, middlePixel, epochs=5) #grayscale = training b&w window , middelPixel = rgb value of middle pixel
-
+print("evaluating...")
 accuracy = model.evaluate(testingGray , testingMiddlePixel)
 print(accuracy)
 
